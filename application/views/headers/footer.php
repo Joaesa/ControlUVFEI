f36ce186090f0e86ee09132cb8979a9322643e74
Joaesa <pumas2005_4@hotmail.com>
2015-04-28 10:12:50 -0500

    Version 2.0.2

--- application/views/headers/footer.php
+++ application/views/headers/footer.php
@@ -9,7 +9,8 @@
 	  <br></br>
 	</div><!--  -->
 	  <footer id="fot"  class="footer navbar-fixed-bottom" style="text-align:center; color:white;">
-	    <div id="fot" class="container">
+	    <div  class="container">
+	    	<p></p>
 	        <p  class="text-muted" style="text-align:center; color:white;">© 2015 <a href="http://www.uv.mx" style="color:black">Universidad Veracruzana</a> All rights reserved.</p>
 	    </div>
 	</footer> 
