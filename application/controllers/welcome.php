df87cab67fae7999aac5e7c8f9a77759cf1445c7
Joaesa <pumas2005_4@hotmail.com>
2015-04-29 10:22:06 -0500

    Version 3.0.1

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,13 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	function __construct()
+ 	{
+   	parent::__construct();
+ 	}
 
 	public function index()
-	{
-		$this->load->view('headers/librerias');
-		$this->load->view('principal');
-		$this->load->view('headers/footer');
-	}
+ 	{
+   	$this->load->helper(array('form'));
+   	$this->load->view('login_view');
+ 	}
+ 
 
 	public function curso(){
 		$this->load->view('headers/librerias');
