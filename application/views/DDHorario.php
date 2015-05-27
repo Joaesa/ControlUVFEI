a6db4c02ac68ae7743937f25c01b4b6261ad43df
valdemar12 <val94rs@gmail.com>
2015-05-27 09:22:24 -0500

    DDHorario Olmos

--- application/views/DDHorario.php
+++ application/views/DDHorario.php
@@ -17,13 +17,16 @@ var id_salon = 0;
 var id_last_drag;
 
 $(document).ready(function(){
-
+repaint_hor();
   id_salon = $('#'+id_salon).html();
   $('#'+id_salon).addClass('active')
   $('.btn_salon').on('click', function() {
-    id_salon = $(this).html();
-    $('#info-salon').html('Salon activo: '+id_salon);
     repaint_hor();
+    id_salon = $(this).html();
+    
+    $('#info-salon').html('Salon activo: '+id_salon);
+
+    
   });
 });
 
@@ -32,8 +35,8 @@ $(document).ready(function(){
   });
 
 function repaint_hor(){
-  $.post('http://localhost/ControlUVFEI/index.php/welcome/get_info_horario', {"salon":id_salon}, function(result){
-    var json = $.parseJSON(result);//analiza una cadena que contiene JSON.
+  $.post('http://localhost:8080/ControlUVFEI/index.php/welcome/get_info_horario', {"salon":id_salon}, function(result){
+    var json = $.parseJSON(result);
     for (var i = json.length - 1; i >= 0; i--) {
       var hora = json[i].Hora;
       var dia = json[i].Dia;
@@ -80,10 +83,11 @@ function drop(ev) {
       var datos = $(ev.target).attr('id').split("-");
       var hora = datos[0];
       var dia = datos[1];
-      $.post('http://localhost/ControlUVFEI/index.php/welcome/BHorario', {"salon":id_salon,"hora":hora,"dia":dia,"nrc":data}, function(result){
+      $.post('http://localhost:8080/ControlUVFEI/index.php/welcome/BHorario', {"salon":id_salon,"hora":hora,"dia":dia,"nrc":data}, function(result){
         console.log(result);
+        
       }).fail(function () {
-        alert('Error AJAX');
+        alert('Error AJAX 1');
       });
     }
     else if( $(ev.target).attr('ocupado') == "false" ){    
@@ -95,10 +99,11 @@ function drop(ev) {
       var dia = datos[1];
       
       $(ev.target).attr('ocupado', "true");
-      $.post('http://localhost/ControlUVFEI/index.php/welcome/web_service', {"salon":id_salon,"hora":hora,"dia":dia,"nrc":data}, function(result){
+      
+      $.post('http://localhost:8080/ControlUVFEI/index.php/welcome/web_service', {"salon":id_salon,"hora":hora,"dia":dia,"nrc":data}, function(result){
         console.log(result);
       }).fail(function () {
-        alert('Error AJAX');
+        alert('Error AJAX 3');
       });
       }
     else{
@@ -156,16 +161,23 @@ function drop(ev) {
       <div class="col-md-2"><label>Miercoles</label></div>
       <div class="col-md-2"><label>Jueves</label></div>
       <div class="col-md-2"><label>Viernes</label></div>
-      <div class="col-md-2"><label>Sabado</label></div>
+    
     </div>
   </div>
 </div>
 <div class="row">
-<div id="curso" class="col-sm-2"  id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+<div id="curso" class="col-sm-2"  id="div0" ondrop="drop(event)"  ondragover="allowDrop(event)">
   <?php 
-    $query =$this->db->get('asignatura');
+    $query =$this->db->get('curso');
       foreach ($query ->result() as $key => $row) {
-        echo '<div id="'.$row->IDA,'" draggable="true" ondragstart="drag(event)">'.$row->Asignatura.'</div>';
+        $IDA=$row->IDA;
+        $this->db->where('IDA',$IDA);
+        $query2 =$this->db->get('asignatura');
+        foreach ($query2 ->result() as $key => $row2) {
+          echo '<div id="'.$row->NRC,'" draggable="true" ondragstart="drag(event)">'.$row2->Asignatura.'</div>';
+    
+        }
+  
       } 
   ?>
 </div>
@@ -196,7 +208,7 @@ function drop(ev) {
 <?php
   for( $i = 1; $i <= 7; $i++ ){
     echo '<div class="row">';
-    for( $j = 1; $j <= 6; $j++ ){
+    for( $j = 1; $j <= 5; $j++ ){
       echo '<div class="col-sm-2">';
       echo '<div id="'.$i.'-'.$j.'"data-id="1" ocupado="false" class="segmento-horario" ondrop="drop(event)" ondragover="allowDrop(event)"></div>';
       echo '</div>';
